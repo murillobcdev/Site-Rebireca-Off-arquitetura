fa57309fae49a6216315a1f1a622398c588feaef
Murillo Brito <murillobdev@gmail.com>
2021-10-25 00:54:33 -0300

    Responsividade 90%, slide com javascript incluso.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,22 +9,45 @@ document.querySelector(".arrow-down").addEventListener("click", function () {
     scrollar();
     console.log("tentou escrollar");
 });
+document.querySelector(".p1").addEventListener("click", function () {
+    scrollPortfolio();
+});
+document.querySelector(".c1").addEventListener("click", function (){
+    scrollContact();
+});
+
+
+/* funções */
+
 function meio() {
     document.querySelector(".banner").classList.remove("b1", "b2");
     document.querySelector(".banner").classList.add("b3");
-    setTimeout(direita, 3000);
+    setTimeout(direita, 2500);
 };
 function direita() {
     document.querySelector(".banner").classList.remove("b2", "b3");
     document.querySelector(".banner").classList.add("b1");
-    setTimeout(esquerda, 3000);
+    setTimeout(esquerda, 2500);
 }
 function esquerda() {
-    document.querySelector(".banner").classList.remove("b3", "b2");
     document.querySelector(".banner").classList.add("b2");
-    setTimeout(meio, 3000);
+    document.querySelector(".banner").classList.remove("b3", "b1");
+    setTimeout(meio, 2500);
 }
+
 function scrollar() {
+    window.scrollTo({
+        top: 1000,
+        behavior: 'smooth',
+    });
+}
+function scrollContact() {
+    window.scrollTo({
+        top: 2000,
+        behavior: 'smooth',
+    });
+}
+function scrollPortfolio() {
     window.scrollTo({
         top: 1000,
         behavior: 'smooth',
