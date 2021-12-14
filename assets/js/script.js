cd3b62840b2a8ec4f43fbc22b776fe613c1fd9ee
Murillo Brito <murillobdev@gmail.com>
2021-12-14 04:13:03 -0300

    teste

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".carousel").addEventListener('slide.bs.carousel', fadeSlide);
+
+function fadeSlide() {
+    let slideList = document.querySelectorAll('.carousel-caption');
+    
+    // let slideArray = Array.from(slideList);
+    // console.log(slideArray[0])
+}
