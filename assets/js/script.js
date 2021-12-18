aa1e2ebd706e4201df7705a779507c5460e83441
Murillo Brito <murillobdev@gmail.com>
2021-12-18 16:17:44 -0300

    Adicionei fade ao carousel, mais projetos e melhorias no portfolio

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".carousel").addEventListener('slide.bs.carousel', fadeSlide);
-
-function fadeSlide() {
-    let slideList = document.querySelectorAll('.carousel-caption');
-    
-    // let slideArray = Array.from(slideList);
-    // console.log(slideArray[0])
-}
